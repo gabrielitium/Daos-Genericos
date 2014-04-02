e3929a5254d6dc6dcd8503daaf968ddb6cde3bc2
gabrielitium <gabrielreyesev@gmail.com>
2014-04-01 20:19:14 -0600

    commit

---
+++ src/mx/gabvax/main/DaosGenericos.java
@@ -0,0 +1,9 @@
+package mx.gabvax.main;
+
+public class DaosGenericos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Daos genéricos");
+	}
+}
